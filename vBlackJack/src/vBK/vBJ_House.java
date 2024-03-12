e55f0da169279ca92a3e82faa5fe46f498983ca9
Ryan <tranryan005@gmail.com>
2024-03-12 08:40:40 -0400

    Created framework for player and house

---
+++ vBlackJack/src/vBK/vBJ_House.java
@@ -0,0 +1,17 @@
+package vBK;
+
+public class vBJ_House extends vB_Deck{
+
+	@Override
+	public String deal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCurrentHand() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ vBlackJack/src/vBK/vBJ_Player.java
@@ -0,0 +1,18 @@
+package vBK;
+
+public class vBJ_Player extends vB_Deck{
+
+	
+	@Override
+	public String deal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCurrentHand() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
