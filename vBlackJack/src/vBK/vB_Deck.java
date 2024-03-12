36691b4aaa2ff06277869d0208993be9fa4fbf37
Ryan <tranryan005@gmail.com>
2024-03-12 08:39:07 -0400

    Added variables to help create framework

--- vBlackJack/src/vBK/vB_Deck.java
+++ vBlackJack/src/vBK/vB_Deck.java
@@ -13,5 +13,15 @@ abstract class vB_Deck implements vBI_Actions{
 			"ACE_H", "2_H", "3_H", "4_H", "5_H", "6_H", "7_H", "8_H", "9_C", "10_H", "Joker_H", "Queen_H", "King_H", // Hearts
 			"ACE_D", "2_D", "3_D", "4_D", "5_D", "6_D", "7_D", "8_D", "9_D", "10_D", "Joker_D", "Queen_D", "King_D", // Diamonds
 			"ACE_S", "2_S", "3_S", "4_S", "5_S", "6_S", "7_S", "8_S", "9_S", "10_S", "Joker_S", "Queen_S", "King_S");// Spades
+	public static int playerCash = 1000;
+	public static int currentBet;
+	
+	/**
+	 * Gets current hand
+	 * @return current hand
+	 */
+	public abstract String getCurrentHand();
+	
+	
 	
 }
