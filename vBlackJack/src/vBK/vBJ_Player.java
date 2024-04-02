8967dc18c6cf8abae3df5f61d71c44a9c46d4944
antho <anthonyferrucci05@gmail.com>
2024-04-02 09:45:46 -0400

    Implemented game functions

--- vBlackJack/src/vBK/vBJ_Player.java
+++ vBlackJack/src/vBK/vBJ_Player.java
@@ -3,21 +3,29 @@ package vBK;
 import java.util.ArrayList;
 
 public class vBJ_Player extends vB_Deck{
+	
+	private ArrayList<String> hand = new ArrayList<>();
+	public int bet;
+	public int balance = 1000;
 
 	
-	@Override
-	public String deal() {
-		
-		return null;
-	}
-
+	/**
+	 * Returns Player's hand
+	 */
 	@Override
 	public ArrayList<String> getCurrentHand() {
-		// TODO Auto-generated method stub
-		return null;
-		//t test
+		return hand;
 	}
-
+	
+	/**
+	 * Retrieves card from deck, gives it to player
+	 */
+	@Override
+	public void hit() {
+		String card = deal();
+		hand.add(card);
+	}
+	
 
 
 }
--- vBlackJack/src/vBK/vB_BlackJackLogic.java
+++ vBlackJack/src/vBK/vB_BlackJackLogic.java
@@ -5,8 +5,40 @@ public class vB_BlackJackLogic {
 	private int[] user;
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		/**
+		 * Testing
+		 */
 
+		vBJ_Player player = new vBJ_Player();
+		vBJ_House house = new vBJ_House();
+		
+		player.hit();
+		house.hit();
+		System.out.println(player.getCurrentHand());
+		System.out.println(house.getCurrentHand());
+		
+		System.out.println(player.eval(player.getCurrentHand()));
+	}
+	
+	/**
+	 * Returns winner of the match
+	 * @param player
+	 * @param house
+	 * @return winner, push
+	 */
+	public static String getWinner(vBJ_Player player, vBJ_House house) {
+		int playerScore = player.eval(player.getCurrentHand());
+		int houseScore = house.eval(house.getCurrentHand());
+		if (playerScore == houseScore) {
+			return "Push";
+		}
+		else if (playerScore > houseScore) {
+			return "Player";
+		}
+		else {
+			return "House";
+		}
 	}
 	
 	
